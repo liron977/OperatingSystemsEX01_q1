f21662696dbc7d8882eeedefe599e0748277e1b9
Liron aviv Kotev <lironkt@mta.ac.il>
2022-11-05 14:29:02 +0200

    Start

---
+++ ex1_q1/ex1_q1.c
@@ -0,0 +1,44 @@
+#include <cstdio>
+int main(int agrc, char* argv[]) {
+    FILE *outputFile;
+    int amountOfFilles = sizeof(argv);
+    char* inputFileName;
+    if (strcmp(argv[1], "-t")==0) {
+        outputFile = fopen("all_std.txt", "w");
+        resize_file("all_std.txt", 0);
+        fseek(outputFile,0,SEEK_SET);
+    }
+    else  if (strcmp(argv[1], "-a") == 0) {
+        outputFile = fopen("all_std.txt", "a");
+    }
+    for (int i = 1; i < amountOfFilles; i++) {
+        inputFileName = argv[i];
+        readDataFromFile(inputFileName, outputFile);
+    }
+}
+void readDataFromFile(char* filleName, FILE* outputFile) {
+    FILE* file = fopen(filleName, "r");
+    char line[80];
+    while (fgets(line, sizeof(line), file)) {
+        getAverageForStudent(line,outputFile);
+    }
+    fclose(file);
+}
+double getAverageForStudent(char* line, FILE* outputFile) {
+    int sumOfGrades = 0;
+    char* token = strtok(line, " ");
+    int index = 0;
+    char* studentName = line[index];
+    int grade = 0;
+    while (token != NULL) {
+        index++;
+        sscanf(token[index], "%d", &grade);
+        sumOfGrades += grade;
+        token = strtok(NULL, " ");
+    }
+    double avg = (double)sumOfGrades / (index - 1);
+    fprintf(outputFile, "%s", studentName);
+    fprintf(outputFile," % .2f", avg);
+    fclose(outputFile);
+}
+
